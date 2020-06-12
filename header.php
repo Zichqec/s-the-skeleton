a7cfde672467316152a496c6b61eee650951f84e
Zichqec <ZichqecArts@gmail.com>
2020-06-12 01:58:43 -0400

    Testing PHP header...

---
+++ header.php
@@ -0,0 +1,8 @@
+<?php
+echo
+    <a href="index.html" class="navlink">Home</a>
+    <a href="s_the_skeleton.html" class="navlink">S the Skeleton</a>
+    <a href="#" class="navlink">Dusty</a>
+    <a href="#" class="navlink">Iea</a>
+    <a href="#" class="navlink">Skryim Guard</a>
+?>
