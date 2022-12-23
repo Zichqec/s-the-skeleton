b13db3eff3dae6078b997d8709044739dabc0f84
Zichqec <36972872+Zichqec@users.noreply.github.com>
2022-12-23 02:05:28 -0500

    Advent Calendar article #01

--- navbar.js
+++ navbar.js
@@ -45,6 +45,7 @@ let links = [
 		{ path: 'resources', title: 'Resources' },
 		{ path: 'advent_calendar_2021', title: 'Advent Calendar 2021' },
 		{ path: 'advent_calendar_2022_00', title: 'Advent Calendar 2022 (#00)' },
+		{ path: 'advent_calendar_2022_01', title: 'Advent Calendar 2022 (#01)' },
 	] },
 ];
 
