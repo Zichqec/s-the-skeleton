534a6ec1bafd532c9e9213b52b7a3b46181a0974
Zichqec <36972872+Zichqec@users.noreply.github.com>
2023-03-23 01:26:26 -0400

    SSP Angel v1.0.0

--- navbar.js
+++ navbar.js
@@ -14,6 +14,7 @@ let links = [
 		{ path: 'xtemplate', title: 'X. Template' },
 		{ path: 'ukatranslator', title: 'Ukatranslator' },
 		{ title: '— Collabs —' },
+		{ path: 'ssp_angel', title: 'SSP Angel' },
 		{ path: 'hydrate', title: 'Hydrate' },
 		{ path: 'paper', title: 'Paper' },
 		{ path: 'needle', title: 'Needle' },
